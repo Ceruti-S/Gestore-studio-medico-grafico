58e3471f3665fb73a6f7f58fb743eb6e90056d53
ceruti <cerutisamuele2008@gmail.com>
2026-05-02 16:17:53 +0200

    finito pom

---
+++ src/main/java/com/studioMedico/GCM/backend/funzionamento/AppLauncher.java
@@ -0,0 +1,13 @@
+package com.studioMedico.GCM.backend.funzionamento;
+
+public class AppLauncher
+{
+
+    public static void main(String[] args)
+    {
+
+        MainClass.main(args);
+
+    }
+
+}
